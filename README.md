4dee779cd4c64c33fd35c92f47ae1765d7aa55fb
Vaishnav Santhosh <vaihome@hotmail.com>
2020-07-23 12:05:14 +0530

    Changed folder structer

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ A basic mp3 player.
 
 # Endpoint
 Basic mp3 player but the point is to learn to use cmake and stuffs.
+
+#Libraries
+Currently using
+- ffmpeg
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// src.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world, finally got this stuff working!" << endl;
+	return 0;
+}
